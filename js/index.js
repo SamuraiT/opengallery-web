d223603e8c972f4c4bfd38a013c08013c0800585
smagch <smagch@gmail.com>
2014-05-02 05:34:51 +0900

    add exhibition details

--- js/index.js
+++ js/index.js
@@ -9,6 +9,26 @@ Backbone.ajax = function(params) {
   return $.ajax(params);
 };
 
-var exhibitions = new ExhibitionCollection();
+// var exhibitions = new ExhibitionCollection();
 // exhibitions.searchByDate('2014-04-24');
 // exhibitions.searchByGalleryId("b9fe1506-30c4-4cff-b73e-99d859199a6d");
+
+var $main = $('main');
+var $details = $('.ex-details');
+
+$main.on('click' ,'.ex-item', function(e) {
+  var $target = $(e.currentTarget);
+  if ($target.is('.selected')) {
+    return;
+  }
+
+  $target
+    .siblings('.selected').removeClass('selected').end()
+    .addClass('selected');
+  $details.addClass('active');
+});
+
+$main.on('click', '.ex-details-close', function (e) {
+  $details.removeClass('active');
+  $('.ex-item.selected').removeClass('selected');
+});
