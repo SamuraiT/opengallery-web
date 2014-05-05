1af32f4f70f1c4795234f21888df93354c7ac0f8
smagch <smagch@gmail.com>
2014-05-05 10:00:09 +0900

    refactor css

--- js/index.js
+++ js/index.js
@@ -14,21 +14,47 @@ Backbone.ajax = function(params) {
 // exhibitions.searchByGalleryId("b9fe1506-30c4-4cff-b73e-99d859199a6d");
 
 var $main = $('main');
-var $details = $('.ex-details');
+var $content = $('.ex-content');
+var $body = $('body');
+var $list = $('.ex-list');
+var scrollTop;
 
-$main.on('click' ,'.ex-item', function(e) {
-  var $target = $(e.currentTarget);
-  if ($target.is('.selected')) {
-    return;
-  }
-
-  $target
+$main.on('click' ,'.ex-item:not(.selected)', function(e) {
+  $(e.currentTarget)
     .siblings('.selected').removeClass('selected').end()
     .addClass('selected');
-  $details.addClass('active');
+  $content.addClass('active');
+
+  var t = $body.scrollTop();
+  $list.css({
+    position: 'fixed',
+    top: -t
+  }).addClass('shrink');
+
+  if (!scrollTop) {
+    scrollTop = t;
+  }
+  $body.scrollTop(0);
 });
 
-$main.on('click', '.ex-details-close', function (e) {
-  $details.removeClass('active');
+$main.on('click', '.ex-item.selected', closeContent);
+
+function closeContent() {
+  $content.removeClass('active');
   $('.ex-item.selected').removeClass('selected');
+  $list.css({
+    position: '',
+    top: ''
+  }).removeClass('shrink');
+  $body.scrollTop(scrollTop);
+  scrollTop = null;
+}
+
+$main.on('click', '.ex-content-close', closeContent);
+
+var $header = $('.header');
+$header.on('click', '.header-calendar-day li:not(.selected)', function (e) {
+  $(e.currentTarget)
+    .siblings('.selected').removeClass('selected').end()
+    .addClass('selected');
 });
